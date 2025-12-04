535f3800f91fa1b20007cc500dc5a0748a2e8f1e
BrandonGomez09 <231205@ids.upchiapas.edu.mx>
2025-12-04 01:13:33 -0600

    Ruta de eventos implementada

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,14 @@ const SERVICE_MAP: Record<string, string> = {
 
   "/api/v1/states": process.env.STATES_SERVICE_URL!,
   "/api/v1/notifications": process.env.NOTIFICATIONS_SERVICE_URL!,
-  "/api/v1/kitchens": process.env.KITCHEN_SERVICE_URL!
+  "/api/v1/kitchens": process.env.KITCHEN_SERVICE_URL!,
+
+  "/api/v1/payments": process.env.PAYMENTS_SERVICE_URL!,
+
+  // 🔥 AGREGA ESTAS 3 LÍNEAS NUEVAS:
+  "/api/v1/events": process.env.EVENTS_SERVICE_URL!,
+  "/api/v1/event-registrations": process.env.EVENTS_SERVICE_URL!,
+  "/api/v1/event-subscriptions": process.env.EVENTS_SERVICE_URL!
 };
 
 app.get("/health", (_req, res) => {
