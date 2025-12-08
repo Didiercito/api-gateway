d4dcbadbb1ae4fda239b5a76507ca20c649a3350
Didiercito <231193@ids.upchiapas.edu.mx>
2025-12-08 04:43:11 -0600

    dd

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import { proxyRequest } from "./utils/proxy.helper";
 import { rateLimitMiddleware } from "./middleware/rate-limit.middleware";
 import { resolveUserLocation } from "./middleware/resolve-location.middleware";
@@ -12,6 +13,7 @@ import { requireRole } from "./middleware/role.middleware";
 dotenv.config();
 
 const app: Application = express();
+
 app.use(cors({ origin: "*" }));
 app.use(helmet());
 app.use(morgan("dev"));
@@ -20,6 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(rateLimitMiddleware);
 
+/**
+ * 🔗 Mapeo de servicios
+ */
 const SERVICE_MAP: Record<string, string> = {
   "/api/v1/auth": process.env.AUTH_USER_SERVICE_URL!,
   "/api/v1/password": process.env.AUTH_USER_SERVICE_URL!,
@@ -35,17 +40,23 @@ const SERVICE_MAP: Record<string, string> = {
   "/api/v1/states": process.env.STATES_SERVICE_URL!,
   "/api/v1/notifications": process.env.NOTIFICATIONS_SERVICE_URL!,
   "/api/v1/kitchens": process.env.KITCHEN_SERVICE_URL!,
-
   "/api/v1/payments": process.env.PAYMENTS_SERVICE_URL!,
-
   "/api/v1/events": process.env.EVENTS_SERVICE_URL!,
   "/api/v1/event-registrations": process.env.EVENTS_SERVICE_URL!,
   "/api/v1/event-subscriptions": process.env.EVENTS_SERVICE_URL!
 };
 
-app.get("/health", (_req, res) => {
-  res.json({ success: true, message: "API Gateway OK" });
+
+app.get("/api/v1/inventory", (req, res) => {
+  proxyRequest(req, res, process.env.INVENTARY_SERVICE_URL!);
 });
+
+app.all(
+  "/api/v1/inventory/*",
+  authenticate,
+  requireRole(["Admin_cocina"]),
+  (req, res) => proxyRequest(req, res, process.env.INVENTARY_SERVICE_URL!)
+);
 
 app.get("/api/v1/kitchens/nearby", resolveUserLocation, (req, res) => {
   proxyRequest(req, res, SERVICE_MAP["/api/v1/kitchens"]);
@@ -72,10 +83,14 @@ app.get(
   (req, res) => proxyRequest(req, res, SERVICE_MAP["/api/v1/kitchens"])
 );
 
-app.all("/api/v1/*", (req: Request, res: Response) => {
-  const path = req.originalUrl;
+app.get("/health", (_req, res) => {
+  res.json({ success: true, message: "API Gateway OK" });
+});
 
-  const prefix = Object.keys(SERVICE_MAP).find((p) => path.startsWith(p));
+
+app.all("/api/v1/*", (req, res) => {
+  const path = req.originalUrl;
+  const prefix = Object.keys(SERVICE_MAP).find(p => path.startsWith(p));
 
   if (!prefix) {
     return res.status(404).json({
